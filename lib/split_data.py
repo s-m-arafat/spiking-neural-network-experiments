4f205de9e3521b7c47398d1832a454a46ce16e5a
s-m-arafat <ahmed.arafat666@gmail.com>
2025-05-25 20:04:21 +0600

    using mnist

--- lib/split_data.py
+++ lib/split_data.py
@@ -1,5 +1,5 @@
 import splitfolders
 
 splitfolders.ratio(
-    "../data_8k/stft_plots", output="../data_8k/stft_plots_org", seed=3, ratio=(0.7, 0.2, 0.1)
+    "../data_8k/mfcc_plots", output="../data_8k/mfcc_plots_org", seed=3, ratio=(0.7, 0.2, 0.1)
 )
